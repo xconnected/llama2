d5b2a47774f0f9cc1785a85607a9f6e9f8dd23f2
xconnected <>
2023-11-15 00:07:15 +0100

    formatting

--- app.py
+++ app.py
@@ -11,48 +11,52 @@ app_port = os.environ.get('PORT')
 app_model = os.environ.get('MODEL')
 
 
-llama_context=2048
-llama_max_tokens=100
-llama_system_message = "You are a helpful assistant and deliver closed sentences"
-llama_prompt_template="<s>[INST] <<SYS>> {system_message} <</SYS>> {user_message} [/INST]"
+context_size = 2048
+max_tokens = 100
+system_message = """You are a helpful assistant"""
+prompt_template = """
+<s>[INST] <<SYS>> {system_message} <</SYS>>
+{user_message}
+[/INST]"""
 
 app = Flask(__name__)
 
-# Just a simple dictionary to simulate a conversation, replace this with your logic
 conversation = []
 
-# define n_ctx manually to permit larger contexts
-LLM = Llama(model_path=app_model, n_ctx=llama_context)
+LLM = Llama(model_path=app_model, n_ctx=context_size)
 
 
-def run_llama(message):
-	input_message = llama_prompt_template.format(system_message=llama_system_message, user_message=message)
-	return LLM(input_message, max_tokens=llama_max_tokens)
+def run_llama(user_message):
+    input_message = prompt_template.format(system_message=system_message,
+                                           user_message=user_message)
+    return LLM(input_message, max_tokens=max_tokens)
 
 
 @app.route('/')
 def index():
-	return render_template("index.html")
-    
+    return render_template("index.html")
+
+
 @app.route('/chat', methods=['POST'])
 def chat():
-	data = request.get_json()
-	message = data.get('message')
+    data = request.get_json()
+    message = data.get('message')
 
-	# Call Llama2 function with the message
-	llama_output = run_llama(message)
+    # Call Llama2 function with the message
+    llama_output = run_llama(message)
 
-	# Extract response
-	response = llama_output['choices'][0]['text']
+    # Extract response
+    response = llama_output['choices'][0]['text']
 
-	# Storing conversation history
-	conversation.append({'user': message, 'bot': response})  
+    # Storing conversation history
+    conversation.append({'user': message, 'bot': response})
+    return ({'message': markdown(response)})
 
-	return ({'message' : markdown(response)})
 
 @app.route('/conversation', methods=['GET'])
 def get_conversation():
-	return jsonify(conversation)
+    return jsonify(conversation)
+
 
 if __name__ == '__main__':
-	app.run(host=app_host, port=app_port, debug=True)
+    app.run(host=app_host, port=app_port, debug=True)
